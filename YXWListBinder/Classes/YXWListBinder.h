affd065e38076af6664d0561eb92ff7872d44e5d
原晓文 <yuanxiao225@icloud.com>
2019-02-18 17:55:11 +0800

    [配置]merge from prepare.

--- YXWListBinder/Classes/YXWListBinder.h
+++ YXWListBinder/Classes/YXWListBinder.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018年 xiaowinner. All rights reserved.
 //
 
-#import <ReactiveObjC/ReactiveObjC.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
 #import "YXWListBinderViewModelProtocol.h"
 #import "YXWListBinderWidgetProtocol.h"
 
