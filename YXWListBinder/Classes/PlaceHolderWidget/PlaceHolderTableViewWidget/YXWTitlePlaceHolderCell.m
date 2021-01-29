471c50f1e7ddb25226fb8ea6d728c847c8741565
xiaowinner <yuanxiao225@vip.qq.com>
2021-01-29 14:02:03 +0800

    [配置]0.4.1

--- YXWListBinder/Classes/PlaceHolderWidget/PlaceHolderTableViewWidget/YXWTitlePlaceHolderCell.m
+++ YXWListBinder/Classes/PlaceHolderWidget/PlaceHolderTableViewWidget/YXWTitlePlaceHolderCell.m
@@ -1,10 +1,12 @@
 #import "YXWTitlePlaceHolderCell.h"
+#import "YXWTitlePlaceHolderViewModel.h"
 
 @interface YXWTitlePlaceHolderCell ()
 
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 
 @end
+
 
 @implementation YXWTitlePlaceHolderCell
 
@@ -13,8 +15,21 @@
     self.selectionStyle = UITableViewCellSelectionStyleNone;
 }
 
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    return self;
+}
+
 - (void)bindViewModel:(id<YXWListBinderViewModelProtocol>)viewModel atIndexPath:(NSIndexPath *)indexPath {
-    
+    YXWTitlePlaceHolderViewModel *vm = (YXWTitlePlaceHolderViewModel *)viewModel;
+    if (vm.atrTitle) {
+        self.titleLabel.attributedText = vm.atrTitle;
+    }else {
+        self.titleLabel.text = vm.title;
+    }
 }
 
 @end
